a944d09255a819d9a404e19a6bda37e8afe1d4dc
James Hoofard <james.hoofard.iii@gmail.com>
2023-06-12 21:02:08 -0500

    fixed the about component so it wouldnt overlap with other components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,28 +5,25 @@ const About = () => {
   return (
     <div name='about' className='w-full h-screen bg-[#0a192f] '>
         <div className='flex flex-col justify-center items-center w-full h-full'>
-            <div className='max-w-[1000px] w-full grid grid-rows-2 gap-8'>
-                <div className='sm:text-right pb-8 pl-4'>
-                    <p className='text-4xl font-bold inline border-b-4 border-[#7b81f2] text-[#f2ec7b]'>
+            <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+                <div className='sm:text-right pb-8 pl-4 '>
+                    <p className='text-4xl inline font-bold border-b-4 border-[#7b81f2] text-[#f2ec7b]'>
                         About
                     </p>
-                    <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8 px-4'>
+                </div>
+                <div></div>
+                </div>
+                <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+                    <div className='sm:text-right text-3xl font-bold text-[#7b81f2]'>
+                        <p>Hi. I'm James, nice to meet you.</p>
                         
-                        <div>
-                            <img className='' src={suitPicture} alt="picture of me, James, in a blue suit" />
-                        </div>
-                        <div>
-                            <p className='py-4 sm:text-right text-xl text-[#7b81f2]'>
-                                I am passionate about building excellent software that improves the lives of those around me. 
-                                I specialize in creating software for clients ranging from indivuals and small businesses all the way to large enterprise corporations.
-                                What would you do if you had an enthusiastic sofware engineer at your fingertips?
-                            </p>
-                        </div>
+                    </div>
+                    <div>
+                        <p className='text-[#f2ec7b] text-xl font-bold'>I'm passionate about building software that improves the lives of those around me. I specialize in creating software for clients ranging from individuals and small businesses all the way up to large corperations. What would you do if you had an enthusiastic software engineer at your fingertips?</p>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
   )
 }
 
