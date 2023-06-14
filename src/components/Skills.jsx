b9ddc45cdb7d39cf79c809503c976d7c75d7014d
James Hoofard <james.hoofard.iii@gmail.com>
2023-06-13 20:28:48 -0500

    completed skills component, added work component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,28 +5,55 @@ import Javascript from '../assets/javascript.png'
 import ReactImg from '../assets/react.png'
 import Node from '../assets/node.png'
 import GraphQL from '../assets/GraphQL_Logo.svg.png'
-import SQL from '../assets/sql-logo.png'
 import Github from '../assets/github.png'
-import Tailwind from '../assets/tailwind.png'
 import MongoDB from '../assets/mongo.png'
 
 const Skills = () => {
   return (
-    <div name='skills' className='bg-[#0a192f]'>
+    <div name='skills' className='w-full h-screen bg-[#0a192f]'>
         {/* Container */}
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full text-[#707525]'>
             <div className=''>
                 <p className='text-4xl font-bold inline border-b-4 border-[#7b81f2]  py-4'>
-                    Experience
+                    Skills
                 </p>
                 <p className='py-6'>
                     These are my primary technologies
                 </p>
             </div>
+            {/* Container for all icons*/}
             <div className='w-full grid grid-cols-2 sm:grid-cols-4 text-center py-8'>
-                <div>
+                <div className='shadow-md shadow-[#420a73] hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto' src={HTML} alt="HTML5 logo" />
-                    <p>HTML</p>
+                    <p className='my-4'>HTML</p>
+                </div>
+                <div className='shadow-md shadow-[#420a73] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={CSS} alt="CSS logo" />
+                    <p className='my-4'>Cascading Style Sheet</p>
+                </div>
+                <div className='shadow-md shadow-[#420a73] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Javascript} alt="Javascript logo" />
+                    <p className='my-4'>Javascript</p>
+                </div>
+                <div className='shadow-md shadow-[#420a73] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={ReactImg} alt="React.js logo" />
+                    <p className='my-4'>React.js</p>
+                </div>
+                <div className='shadow-md shadow-[#420a73] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={MongoDB} alt="MongoDB logo" />
+                    <p className='my-4'>MongoDB</p>
+                </div>
+                <div className='shadow-md shadow-[#420a73] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Github} alt="Github logo" />
+                    <p className='my-4'>Github</p>
+                </div>
+                <div className='shadow-md shadow-[#420a73] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Node} alt="Node.js logo" />
+                    <p className='my-4'>Node.js</p>
+                </div>
+                <div className='shadow-md shadow-[#420a73] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={GraphQL} alt="GraphQL logo" />
+                    <p className='my-4'>GraphQL</p>
                 </div>
             </div>
         </div>
---
+++ src/components/Work.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div className='w-full md:h-screen text-[#7b81f2] bg-[#0a192f]'>
+
+    </div>
+  )
+}
+
+export default Work
